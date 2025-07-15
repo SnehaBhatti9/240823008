b515399c4c5a727d2fdfe6988d305b316f68bc28
SnehaBhatti9 <snehabhatti1803@gmail.com>
2025-07-15 13:32:48 +0530

    Second Program

---
+++ second.js
@@ -0,0 +1,9 @@
+const val1=5000
+const val2=1000
+if(val1>val2){
+    console.log(`${val1} is greater than ${val2}`)
+}else if(val2>val1){
+    console.log(`${val2} is greater than ${val1}`)
+}else{
+    console.log("Bot are same")
+}
