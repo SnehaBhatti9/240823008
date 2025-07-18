b7bd618f0bf2124e98d3e851ba3a78cba220f719
SnehaBhatti9 <snehabhatti1803@gmail.com>
2025-07-18 10:45:39 +0530

    Programs

---
+++ Program4.js
@@ -0,0 +1,7 @@
+// sum of natural numbers
+const num=5
+let sum=0
+for(i=1;i<=num;i++){
+    sum += i    
+}
+console.log(sum)
---
+++ Program6.js
@@ -0,0 +1,5 @@
+let num = 10; 
+for(i=1;i<=10;i++){
+    let mul=num*i
+    console.log(`${num} * ${i} = ${mul}`)
+}
